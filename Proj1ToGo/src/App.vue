5a81d9c6b8928e85e42dc401496ecdb6df77309c
wei tao <stevenlt4@hotmail.com>
2017-10-24 15:54:58 +0800

    vue togo app with header complete

---
+++ Proj1ToGo/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods" class=tabs>商品</router-link>
+        </div>
+      <div class="tab-item">
+        <router-link to="/ratings" class=tabs>评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller" class=tabs>商家</router-link>
+    </div>
+  </div>
+  <router-view></router-view>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+import header from './components/header/header.vue'
+
+const ERR_OK = 0
+
+export default {
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  created () {
+    this.$http.get('/api/seller').then((response) => {
+      response = response.body
+      if (response.errno === ERR_OK) {
+        this.seller = response.data
+      }
+    })
+  },
+  components: {
+    'v-header': header
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex:1
+      text-align: center
+    & .tabs
+      display:block
+      color: rgb(77, 85, 93)
+      &.active
+        color: rgb(240, 20, 20)
+</style>
---
+++ Proj1ToGo/src/components/seller/seller.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+
+selllllllleeer
+
+</div>
+</template>
+
+
+<script type="text/ecmascript-6">
+	export default{
+
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus"></style>
